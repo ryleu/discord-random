196e405b1ea17cc65909bcfb08bfbc7838853752
FourInchKnife <riley.j.mclain@gmail.com>
2021-04-13 09:36:29 -0400

    added start.sh

--- run.py
+++ run.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python3
-from discord.ext.commands import Bot, when_mentioned
-from json import loads
+from discord.ext import commands
+import json
 import logging
-from traceback import format_tb
-from discord.ext.commands.errors import CommandNotFound
-from discord_slash import SlashCommand, SlashContext
-from datetime import datetime
-from discord import Activity, ActivityType, CustomActivity
+import traceback
+import discord_slash
+import datetime
+import discord
 
 formatTime = lambda time: str(time)[:str(time).index(".")].replace(":","-").replace(" ","_")
 try:
-    logging.basicConfig(filename=f'logs/{formatTime(datetime.now())}.log', level=logging.INFO)
+    logging.basicConfig(filename=f'logs/{formatTime(datetime.datetime.now())}.log', level=logging.INFO)
 except FileNotFoundError:
     logging.warning("No directory 'logs/' in the current working directory. Errors will not be saved.")
 
 with open("token.json","r") as file:
-    config = loads(file.read())
+    config = json.loads(file.read())
 
 owners = config.pop("owners",None)
 
-me = Bot(when_mentioned,owner_ids=set(owners) if owners else None,
-    activity = CustomActivity(name = "Secretly put this as your status and don’t mention it to me or anyone and we can watch it silently spread."))
-me.slash_handler = SlashCommand(me, sync_on_cog_reload = True, override_type = True, delete_from_unused_guilds = True)
+me = commands.Bot(commands.when_mentioned,owner_ids=set(owners) if owners else None,
+    activity = discord.CustomActivity(name = "Secretly put this as your status and don’t mention it to me or anyone and we can watch it silently spread."))
+me.slash_handler = discord_slash.SlashCommand(me, sync_on_cog_reload = True, override_type = True, delete_from_unused_guilds = True)
 
 me.load_extension("extensions")
 
-async def rlext(ctx: SlashContext):
+async def rlext(ctx: discord_slash.SlashContext):
     """Reloads the commands."""
     await ctx.defer(hidden = True)
     if await me.is_owner(ctx.author):
         try:
             me.reload_extension("extensions")
         except Exception as err:
-            logging.critical("\n".join(format_tb(err.__traceback__)))
+            logging.critical("\n".join(traceback.format_tb(err.__traceback__)))
             await ctx.send("wow you killed me great thanks", hidden = True)
         else:
             await ctx.send("Reloaded all commands.", hidden = True)
@@ -44,11 +43,11 @@ me.slash_handler.add_slash_command(rlext)
 
 @me.event
 async def on_slash_command_error(ctx,err):
-    logging.exception("\n".join(format_tb(err.__traceback__)))
+    logging.exception("\n".join(traceback.format_tb(err.__traceback__)))
 
 @me.event
 async def on_command_error(ctx,err):
-    if isinstance(err,CommandNotFound):
+    if isinstance(err,commands.errors.CommandNotFound):
         return
     logging.exception(err)
     await ctx.send(str(err))
