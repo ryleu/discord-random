c39ede87a9536da17be01648ed5f36e1ef1e2e0f
ryleu <riley.j.mclain@gmail.com>
2021-10-28 12:42:16 -0400

    Change the name of the links

--- info_extension.py
+++ info_extension.py
@@ -14,14 +14,14 @@ class Info(commands.Cog):
         # Create an embed to store the info
         embed = discord.Embed(title="D&D Random Info", color=discord.Color(0x00FFFF))
         # Add the source code link to the embed
-        embed.add_field(name="Source Code", value="[Click here to view the source code.]("
+        embed.add_field(name="Source Code", value="[View on GitHub]("
                                                   "https://github.com/ryleu/discord-cardic-inspiration)")
         # Add the invite link to the embed
-        embed.add_field(name="Invite", value="[Click here to invite me.]("
+        embed.add_field(name="Invite", value="[Add me to your own server]("
                                              "https://discord.com/api/oauth2/authorize?client_id=803318907950596106"
                                              "&permissions=0&scope=applications.commands%20bot)")
         # Add the syntax link to the embed
-        embed.add_field(name="Syntax", value="[Click here to see the syntax.]("
+        embed.add_field(name="Syntax", value="[View the `/roll` syntax]("
                                              "https://github.com/avrae/d20#dice-syntax)")
         # Add the bot author to the embed
         embed.set_footer(text="Created by ryleu#0001")
