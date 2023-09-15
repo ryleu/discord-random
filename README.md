1c1c387a0a1033d5c10a836e47d967c9b1b96a81
ryleu <69326171+ryleu@users.noreply.github.com>
2023-09-15 01:39:54 -0500

    initial port to interactions.py 5.10.0

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# discord-cardic-inspiration
+# discord-random
 
-[![Codacy Badge](https://api.codacy.com/project/badge/Grade/eaaba74ddd05435993469b8cc7bc85b3)](https://app.codacy.com/gh/FourInchKnife/discord-cardic-inspiration?utm_source=github.com&utm_medium=referral&utm_content=FourInchKnife/discord-cardic-inspiration&utm_campaign=Badge_Grade_Settings)
-
-implements cardic inspiration in discord servers
+rolls dice and maybe will do other things in the future
--- run.py
+++ run.py
@@ -1,82 +1,102 @@
-#!/usr/bin/env python3
-import datetime
-import json
-import logging
+#!/bin/env -S poetry run python3
+
+import interactions
 import os
-import traceback
+import d20
+import random
 
-import discord
-import discord_slash
-from discord.ext import commands
+bot = interactions.Client()
 
+# get the token. first try envars then try token.txt
+token = os.environ.get("TOKEN", False)
 
-me = commands.Bot(commands.when_mentioned)
-
-
-def format_time(time):
-    return str(time)[:str(time).index(".")].replace(":", "-").replace(" ", "_")
-
-
-async def rlext(ctx: discord_slash.SlashContext):
-    """Reloads the commands."""
-    await ctx.defer(hidden=True)
-    if await me.is_owner(ctx.author):
-        try:
-            me.reload_extension("extensions")
-        except Exception as err:
-            logging.critical("\n".join(traceback.format_tb(err.__traceback__)))
-            await ctx.send("wow you killed me great thanks", hidden=True)
-        else:
-            await ctx.send("Reloaded all commands.", hidden=True)
-        print("\n-----RELOAD-----\n")
-    else:
-        await ctx.send("You need to be the owner of the bot to use this command.", hidden=True)
+if not (token):
+    with open("token.txt") as file:
+        token = file.read().strip()
 
 
-def main():
+@bot.listen()
+async def on_startup():
+    print("Bot is ready!")
+
+
+@bot.command(name="info", description="Send information about this bot.")
+async def info(ctx: interactions.SlashContext):
+    """Displays the info of the bot."""
+    await ctx.defer()
+    # Create an embed to store the info
+    embed = interactions.Embed(
+        title="D&D Random Info", color=interactions.Color(0x00FFFF)
+    )
+    # Add the source code link to the embed
+    embed.add_field(
+        name="Source Code",
+        value="[View on GitHub]("
+        "https://github.com/ryleu/discord-cardic-inspiration)",
+    )
+    # Add the invite link to the embed
+    embed.add_field(
+        name="Invite",
+        value="[Add me to your own server]("
+        "https://discord.com/api/oauth2/authorize?client_id=803318907950596106"
+        "&permissions=0&scope=applications.commands%20bot)",
+    )
+    # Add the syntax link to the embed
+    embed.add_field(
+        name="Syntax",
+        value="[View the `/roll` syntax](" "https://github.com/avrae/d20#dice-syntax)",
+    )
+    # Add the bot author to the embed
+    embed.set_footer(text="Created by ryleu")
+
+    # Send the embed
+    await ctx.send(embed=embed)
+
+
+@bot.command(
+    name="roll",
+    description="Rolls dice using d20's roll string format.",
+    options=[
+        interactions.SlashCommandOption(
+            name="roll_string", type=interactions.OptionType.STRING, required=True
+        )
+    ],
+)
+async def roll(ctx: interactions.SlashCommand, roll_string: str):
     try:
-        file_name = f'logs/{format_time(datetime.datetime.now())}.log'
-        logging.basicConfig(filename=file_name, level=logging.INFO)
-        print("Saving logs to", file_name)
-    except FileNotFoundError:
-        logging.warning("No directory 'logs/' in the current working directory. Errors will not be saved.")
+        roll_result = str(d20.roll(roll_string))
+    except d20.errors.TooManyRolls:
+        roll_result = "You can't roll more than 1000 total dice."
+    except d20.errors.RollSyntaxError as err:
+        escaped_params = roll_string.replace("`", "\\`")
+        roll_result = f"""{str(err)}```\n{escaped_params}\n{(err.col - 1) * " " + len(err.got) * "^"}\n```"""
+    except Exception as err:
+        roll_result = str(err)
 
-    try:
-        token = os.environ["BOT_TOKEN"]
-        logging.info("Found BOT_TOKEN in environment variables.")
-    except KeyError:
-        with open("token.json", "r") as file:
-            token = json.loads(file.read())["token"]
-
-    me.slash_handler = discord_slash.SlashCommand(
-        me, sync_commands=True, sync_on_cog_reload=True, override_type=True, delete_from_unused_guilds=True)
-    me.allowed_mentions = discord.AllowedMentions.none()
-    me.help_command = None
-
-    me.load_extension("card_extension")
-    me.load_extension("dice_extension")
-    me.load_extension("miscibility_extension")
-    me.load_extension("info_extension")
-
-    me.slash_handler.add_slash_command(rlext)
-
-    @me.event
-    async def on_slash_command_error(ctx, err):
-        logging.exception("\n".join(traceback.format_tb(err.__traceback__)))
-
-    @me.event
-    async def on_command_error(ctx, err):
-        if isinstance(err, commands.errors.CommandNotFound):
-            return
-        logging.exception(err)
-        await ctx.send(str(err))
-
-    @me.event
-    async def on_ready():
-        print(f"Logged in as {me.user}")
-
-    me.run(token)
+    await ctx.send(str(roll_result), ephemeral=True)
 
 
-if __name__ == "__main__":
-    main()
+@bot.command(
+    name="init_channel", description="Set up a channel for rolling dice with buttons."
+)
+async def init_channel(ctx: interactions.SlashContext):
+    print(ctx.__dict__)
+    buttons = interactions.spread_to_rows(
+        *[
+            interactions.Button(
+                style=interactions.ButtonStyle.PRIMARY, label=f"d{x}", custom_id=str(x)
+            )
+            for x in (2, 4, 6, 8, 10, 12, 20, 100)
+        ]
+    )
+    await ctx.send("Press a button to roll a die:", components=buttons)
+
+
+@bot.listen()
+async def button_pressed(event: interactions.events.ButtonPressed):
+    ctx = event.ctx
+    number = int(ctx.custom_id)
+    await ctx.send(f"d{number} = `{random.randint(1, number)}`", ephemeral=True)
+
+
+bot.start(token)
